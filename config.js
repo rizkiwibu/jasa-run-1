8bf87fa9683e4950495e0e76eff441a23f1d23e1
Rizkikun <101055276+Rizkikun@users.noreply.github.com>
2022-04-10 08:47:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ gc1 = ''
 gc2 = ''
 gc3 = ''
 global.linkGC = ['https://chat.whatsapp.com/BDF5D0ShUcpKRLWrlioD3E', ''] // ganti jadi group lu
-global.owner = ['6283189357509','6283189357509'] // Put your number here //owner eval
-global.kontak = ['6283189357509','6283189357509'] //Ketika ada yang ngetik #owner
+global.owner = ['628179478764','628179478764'] // Put your number here //owner eval
+global.kontak = ['628179478764','628179478764'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
 global.prems = ['6283817161861','601164215501','60166408295','601161197507','601156232697','601172962735','60182387590','60109211153','6283817161861'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -49,21 +49,21 @@ xteamkey = 'Dawnfrostkey'
 namaig = 'GAK ADA :)'
 namagithub = 'https://github.com/rizkiwibu'
 kasihcaption = `Nih kak`
-namakontak1 = 'aellsm Owner Bot'
+namakontak1 = 'DDOEDITZYT'
 namakontak2 = ' owner Bot'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'kenxie Botz' // ganti aja
-global.author = 'kenxie Botz' // ganti aja
+global.packname = '𝙴𝚕𝚊𝚒𝚗𝚊𝙱𝚘𝚝𝚣' // ganti aja
+global.author = '𝙴𝚕𝚊𝚒𝚗𝚊𝙱𝚘𝚝𝚣' // ganti aja
 
 //yyy
-bc = 'kenxie Botz' //kenxie Botz Broadcast
-footer = '\n© ikyBotz'
-namabot = 'kenxie Botz'
+bc = '𝙴𝚕𝚊𝚒𝚗𝚊𝙱𝚘𝚝𝚣' //𝙴𝚕𝚊𝚒𝚗𝚊𝙱𝚘𝚝𝚣 Broadcast
+footer = '\n© 𝙴𝚕𝚊𝚒𝚗𝚊𝙱𝚘𝚝𝚣'
+namabot = '𝙴𝚕𝚊𝚒𝚗𝚊𝙱𝚘𝚝𝚣'
 namalu = 'kenxie'
-watermark = 'kenxie Botz' //ganti aja
+watermark = '𝙴𝚕𝚊𝚒𝚗𝚊𝙱𝚘𝚝𝚣' //ganti aja
 
 // 
 wait = '_*Tunggu Sebentar...*_'
